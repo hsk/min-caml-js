ec36dd847ba8531b6640b833ea904b79f93567c4
Hiroshi Sakurai <sakurai777@gmail.com>
2015-02-14 16:29:54 +0900

    added Printf

--- libs/lib.js
+++ libs/lib.js
@@ -17,6 +17,9 @@ function truncate(a) {
 function float_of_int(a){
 	return a+0.0;
 }
+function int_of_char(a){
+	return a;
+}
 function print_newline(){
 	console.log("");
 }
@@ -32,8 +35,67 @@ String.concat = function(sep, ls) {
 	if(ls.data._1.tag=="Nil") return ls.data._0;
 	return ls.data._0 + sep + String.concat(sep, ls.data._1)
 }
+
+String.uncapitalize = function(str) {
+	return str.charAt(0).toLowerCase() + str.slice(1);
+}
+String.capitalize = function(str) {
+	return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+String.get = function(str,pos) {
+	return str.charCodeAt(pos);
+};
+
 Array.create = function (n,v) {
 	var a = [];
 	for(var i = 0; i < n; i++) a[i] = v;
 	return a;
+};
+
+var Format = {
+	fprintf: function(fp,s) {
+		s = s.replace(/@\./g,"\n").replace(/@?/g,"");
+		var n = 2;
+		var arg = arguments;
+		return fp(s.replace(/%(.)/g, function(a, b) {
+			switch (b) {
+				case "a":
+					var f = arg[n++];
+					return f(function(e){return e;},arg[n++]);
+				case "f":
+					return arg[n++].toFixed(6);
+				default: return arg[n++];
+			}
+
+		}));
+	},
+	sprintf: function(s) {
+		var args = Array.prototype.slice.call(arguments);
+	    args.unshift(function(i){return i;});
+		return Format.fprintf.apply(this,args);
+	},
+	printf: function() {
+		var args = Array.prototype.slice.call(arguments);
+	    args.unshift(print_string);
+		Format.fprintf.apply(this,args);
+	},
+};
+
+var Printf = {
+	sprintf: function(s) {
+		var n = 1;
+		var arg = arguments;
+		return s.replace(/%(.)/g, function(a, b) {
+			switch (b) {
+				case "f":
+					return arg[n++].toFixed(6);
+				default: return arg[n++];
+			}
+
+		});
+	},
+	printf: function() {
+		print_string(Printf.sprintf.apply(this,arguments));
+	},
 };
--- test/string.ml
+++ test/string.ml
@@ -4,4 +4,23 @@ print_string (String.sub "ABC" 1 1);
 print_string (String.sub "ABC" 0 3);
 print_int (String.length "ABC");
 print_string (String.concat ";" ["a";"b";"c"]);
-print_newline()
+print_string (String.capitalize "abcd");
+print_string (String.uncapitalize "Abcd");
+print_int (int_of_char(String.get "123" 0));
+print_newline();
+
+Printf.printf "a\n";
+
+Printf.printf "%s\n" "hoge";
+Printf.printf "%d\n" 111;
+
+Printf.printf "%f\n" 111.0;
+
+Printf.printf "%f\n" 111.123456;
+
+let rec f1 oc e =
+  Format.fprintf oc "%f\n" e
+in
+Format.printf "%a@." f1 1.23;
+let s = (Format.sprintf "%f@." 1.23) in
+Format.printf "%s@." s
